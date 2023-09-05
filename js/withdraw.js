505e75bcaf1efe12f0992f902d36532a05d084e6
Kushalghosh1 <cse.kushal1@gmail.com>
2023-09-05 21:42:34 +0600

    withdraw section updated

--- js/withdraw.js
+++ js/withdraw.js
@@ -18,6 +18,14 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    // step 7
+    withdrawField.value = '';
+
+    if(isNaN(newWithdrawAmount)){
+        alert('Please provide a valid number');
+        return;
+    }
     //step 3
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
@@ -32,13 +40,17 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert('Insufficient Balance!');
+        return;
+    }
+
     //step 6
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
 
-    // step 7
-    withdrawField.value = '';
+   
 
 
 
