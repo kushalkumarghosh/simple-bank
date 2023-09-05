92258d0cf3237c4802c3b3fddca841cef104e016
Kushalghosh1 <cse.kushal1@gmail.com>
2023-09-05 21:21:03 +0600

    finished the withdraw section

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 // step-1: add click event handler with the submit button
-document.getElementById('btn-submit').addEventListener('click', function(){
+document.getElementById('btn-submit').addEventListener('click', function () {
     //step-2: get the email address inside the email input field
     //always remember to use .value to get text from an input field
     const emailField = document.getElementById('user-email');
@@ -11,13 +11,13 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     //step-3.c: get the value from the element
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
-    
+
     //DANGER: do not verify email password on the client side
     //step-4: verify email and password and check whether valid user or not
-    if(email === 'cse.kushal1@gmail.com' && password === '1234'){
-        window.location.href='bank.html';
+    if (email === 'cse.kushal1@gmail.com' && password === '1234') {
+        window.location.href = 'bank.html';
     }
-    else{
+    else {
         alert('Invalid user');
     }
 
