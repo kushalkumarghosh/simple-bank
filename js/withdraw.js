9cba1e693ae6fc284ed9a0b7a6f50c07d5014782
Kushalghosh1 <cse.kushal1@gmail.com>
2023-09-05 22:27:55 +0600

    withdraw.js updated

--- js/withdraw.js
+++ js/withdraw.js
@@ -18,11 +18,12 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    console.log(newWithdrawAmount);
 
     // step 7
     withdrawField.value = '';
 
-    if(isNaN(newWithdrawAmount)){
+    if (isNaN(newWithdrawAmount)) {
         alert('Please provide a valid number');
         return;
     }
@@ -31,30 +32,23 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    //step 4
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
-
     //step 5
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
-    if(newWithdrawAmount > previousBalanceTotal){
+    if (newWithdrawAmount > previousBalanceTotal) {
         alert('Insufficient Balance!');
         return;
     }
 
+    //step 4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     //step 6
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
-
-
-   
-
-
-
-
 
 
 })
